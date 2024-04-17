472ed5a65f43b2a0ea8e16fcdd57d8b48ca97da5
gazakorompis <grkorompis@gmail.com>
2024-04-18 00:59:00 +0700

    feat: #2-fe-frontpage adding color draping and animation

--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -1,17 +1,77 @@
+import { useEffect, useState } from "react";
 import { AppNavbar } from "../../components";
 import "./landing-page.css"
 
 const LandingPage = () => {
+        const [isPageReady, setIsPageReady] = useState(false);
 
+        useEffect(()=>{
+            const timer = setTimeout(()=>{
+                setIsPageReady(true)
+            }, 800)
+        }, [])
         return(
             <>
                 <div className="app-pages page-landing">
                     <AppNavbar/>
-                    <div className="page-section main">
+                    <div className="page-section banner">
 
                     </div>
-                    <div className="page-section collection-carousel">
+                    <div className={("page-section color-draping") + (isPageReady ? " section-show" : "")}>
+                        <h1>color draping</h1>
+                        <div className="conveyor">
+                            <div className="conveyor-item sliding-content ">
+                               
+                            </div>
+                            <div className="conveyor-item sliding-content ">
 
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                             <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                            <div className="conveyor-item sliding-content ">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="page-section collection-carousel">
+                        
                     </div>
                 </div>
             </>
